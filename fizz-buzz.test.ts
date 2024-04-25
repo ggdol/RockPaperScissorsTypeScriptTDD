d186fb67aa87274e85e902f1608c6f7ed5d332de
ggdol <dolgg0244@gmail.com>
2024-04-25 10:31:30 +0400

    add is prime method

--- fizz-buzz.test.ts
+++ fizz-buzz.test.ts
@@ -3,9 +3,9 @@ import { createFizzBuzz } from "./fizz-buzz";
 describe('fizz-buzz', ()=>{
     describe('fizz', ()=>{
         test.each([
-            {input:3},
             {input:6},
             {input:9},
+            {input:12},
         ])('$input', ({input})=>{
             //Arrange
             const expected = "Fizz";
@@ -24,9 +24,9 @@ describe('fizz-buzz', ()=>{
 
     describe('buzz', ()=>{
         test.each([
-            {input:5},
             {input:10},
             {input:20},
+            {input:25},
         ])('$input', ({input})=>{
             //Arrange
             const expected = "Buzz";
@@ -42,4 +42,108 @@ describe('fizz-buzz', ()=>{
         })
 
     })
+
+    describe('fizzbuzz', ()=>{
+        test.each([
+            {input:15},
+            {input:30},
+            {input:45},
+        ])('$input', ({input})=>{
+            //Arrange
+            const expected = "FizzBuzz";
+            const sut = createFizzBuzz();
+
+            //Act
+
+            const actual = sut.go(input);
+
+            //Assert
+
+            expect(actual).toBe(expected);
+        })
+
+    })
+
+    describe('number itself', ()=>{
+        test.each([
+            {input:1, expected:"1"},
+            {input:4, expected:"4"},
+            {input:14, expected:"14"},
+            {input:76, expected:"76"},
+            {input:169, expected:"169"},
+            // {input:45},
+        ])('$input', ({input, expected})=>{
+            //Arrange
+            const sut = createFizzBuzz();
+
+            //Act
+
+            const actual = sut.go(input);
+
+            //Assert
+
+            expect(actual).toBe(expected);
+        })
+
+    })
+
+    describe('fizzwhiz', ()=>{
+        test.each([
+            {input:3},
+        ])('$input', ({input})=>{
+            //Arrange
+            const expected = "FizzWhiz";
+            const sut = createFizzBuzz();
+
+            //Act
+
+            const actual = sut.go(input);
+
+            //Assert
+
+            expect(actual).toBe(expected);
+        })
+
+    })
+
+    describe('buzzwhiz', ()=>{
+        test.each([
+            {input:5},
+        ])('$input', ({input})=>{
+            //Arrange
+            const expected = "BuzzWhiz";
+            const sut = createFizzBuzz();
+
+            //Act
+
+            const actual = sut.go(input);
+
+            //Assert
+
+            expect(actual).toBe(expected);
+        })
+
+    })
+
+
+    describe('whiz', ()=>{
+        test.each([
+            {input:2},
+            {input:7},
+        ])('$input', ({input})=>{
+            //Arrange
+            const expected = "Whiz";
+            const sut = createFizzBuzz();
+
+            //Act
+
+            const actual = sut.go(input);
+
+            //Assert
+
+            expect(actual).toBe(expected);
+        })
+
+    })
+
 })
--- fizz-buzz.ts
+++ fizz-buzz.ts
@@ -5,10 +5,24 @@ interface FizzBuzz{
 
 export function createFizzBuzz(): FizzBuzz{
 
+    function isPrime(num: number): boolean{
+        if(num === 1) return false;
+        for(let factor = 2; factor <= Math.sqrt(num); factor += 1){
+            if(num % factor === 0 && num > factor) return false;
+        }
+        return true;
+
+    }
     return{
         go: function(num: number): string {
+            
+            if(num === 3) return "FizzWhiz";
+            if(num === 5) return "BuzzWhiz";
+            if(num % 15 === 0) return "FizzBuzz";
             if(num % 5 === 0) return "Buzz";
-            return "Fizz";
+            if(num % 3 === 0) return "Fizz";
+            if(isPrime(num)) return "Whiz";
+            return num.toString();
         }
     }
 }
